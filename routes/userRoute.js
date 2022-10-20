a0281e9edff821bde7d6641b0c1306670b5c450c
nstutorial <nsutradhar.cob@gmail.com>
2022-10-20 17:10:52 +0530

    2010220510

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,13 +5,17 @@ const user_route = express();
 user_route.use(bodyParser.json());
 user_route.use(bodyParser.urlencoded({extended:true}));
 
+const session = require('express-session');
+const config = require('../config/config')
+user_route.use(session({secret:config.sessionSecret}));
+
 user_route.set('view engine','ejs');
 user_route.set('views','./views');
 
 user_route.use(express.static('public'));
 
-user_route.get("/",(req,res)=>{
-    res.send("USER ROUTE")
-})
+const userController = require("../controllers/userController");
+
+user_route.get("/login",userController.loadLogin);
 
 module.exports =user_route;
